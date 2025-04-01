f7922af0cfaf9db8c3afaa5d30b6f9986b56dafa
Daniel <d.kummel@penterep.com>
2025-04-01 14:54:33 +0200

    rm

--- ptwordpress/modules/write_to_file.py
+++
@@ -1,6 +0,0 @@
-def write_to_file(filename, content):
-    try:
-        with open(filename, 'a') as file:
-            file.write(content + '\n')
-    except Exception as e:
-        print(f"Error writing to file: {e}")
